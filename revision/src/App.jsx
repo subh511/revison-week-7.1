c12b9eeb7531c42aef9f7fb1dfa5c52046ba6a9d
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-03 21:18:25 +0530

    done props drilling with context api

--- revision/src/App.jsx
+++ revision/src/App.jsx
@@ -1,51 +1,86 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-const Landing = lazy(() => import("./components/Landing.jsx"));
-const Dashboard = lazy(() => import("./components/Dashboard.jsx"));
+// import { lazy, Suspense } from "react";
+// import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+// const Landing = lazy(() => import("./components/Landing.jsx"));
+// const Dashboard = lazy(() => import("./components/Dashboard.jsx"));
+
+import { useContext, useState } from "react";
+import { CountContext } from "./components/ContextApi";
 
 function App() {
+
+  const [count,setCount] = useState(0)
+
   return (
     <div>
-      <BrowserRouter>
-        <Appbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={"Loading..."}>
-                <Landing />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/description"
-            element={
-              <Suspense fallback={"Loading..."}>
-                <Dashboard />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+    <CountContext.Provider value={count}>
+    <Count setCount={setCount}/>
+    </CountContext.Provider>
     </div>
   );
 }
 
-function Appbar() {
-  const navigate = useNavigate();
-  //{navigate("/")}
-  //{navigate("/description")}
-  // window.location.href = "/";
-  //window.location.href = "/description";
-  return (
+function Count({setCount}){
+  return(
     <div>
-      <div>
-        <button onClick={() => navigate("/")}>Landing-Pages</button>
-        <button onClick={() => navigate("/description")}>
-          Description-Page
-        </button>
-      </div>
+    <CountRenderer/>
+    <Button setCount={setCount}/>
     </div>
-  );
+  )
 }
+function CountRenderer(){
+  const count = useContext(CountContext)
+  return(
+    <div>{count}</div>
+  )
+}
+function Button({setCount}){
+
+  const count = useContext(CountContext)
+  return(
+    <div>
+    <button onClick={()=> setCount(count+1)}>add</button>
+    <button onClick={()=> setCount(count-1)}>Subtract</button>
+    </div>
+  )
+}
+
+// <BrowserRouter>
+// <Appbar />
+// <Routes>
+//   <Route
+//     path="/"
+//     element={
+//       <Suspense fallback={"Loading..."}>
+//         <Landing />
+//       </Suspense>
+//     }
+//   />
+//   <Route
+//     path="/description"
+//     element={
+//       <Suspense fallback={"Loading..."}>
+//         <Dashboard />
+//       </Suspense>
+//     }
+//   />
+// </Routes>
+// </BrowserRouter>
+
+// function Appbar() {
+//   const navigate = useNavigate();
+//   //{navigate("/")}
+//   //{navigate("/description")}
+//   // window.location.href = "/";
+//   //window.location.href = "/description";
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={() => navigate("/")}>Landing-Pages</button>
+//         <button onClick={() => navigate("/description")}>
+//           Description-Page
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 export default App;
